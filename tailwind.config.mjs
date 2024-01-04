b06a3dbdd8a1b8225afa4bb71ba2162fc5c46558
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2024-01-03 19:51:45 -0700

    Add navbar, footer, and implement dark mode toggle.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    fontSize: {
+      title: ["clamp(3rem, 14vw, 9rem)"],
+      "heading-1": ["clamp(3rem, 10vw, 10rem)"],
+      "heading-2": ["clamp(2.5rem, 6.5vw, 10rem)"],
+      "heading-3": ["clamp(2.4rem, 8vw, 10rem)"],
+      "heading-4": ["clamp(2rem, 5vw, 2.75rem)"],
+      "works-title": ["clamp(1.25rem, 2vw, 1.5rem)"],
+      "body-1": ["clamp(1.1rem, 2vw, 1.3rem)"],
+      "body-2": ["clamp(1rem, 1.5vw, 1.5rem)"],
+      "body-3": "1.1rem",
+      "body-4": ["clamp(0.75rem, 3vw, 1rem)"],
+    },
+    letterSpacing: {
+      headings: "-0.03em",
+    },
+    fontFamily: {
+      general: ["GeneralSans-Variable", "sans-serif"],
+      grotesk: ["CabinetGrotesk-Variable", "sans-serif"],
+    },
+    colors: {
+      black: "#262626",
+      white: "#FFFFFF",
+      transparent: "transparent",
+      "primary-200": "#F2F2F2",
+      "primary-300": "#E6E6E6",
+      "primary-400": "#D9D9D9",
+      "accent-100": "#FAFAF9",
+      "accent-200": "#E8E8E3",
+      "accent-300": "#DDDDD5",
+      "accent-400": "#D1D1C7",
+      "accent-500": "#AEAE9D",
+      "accent-600": "#8C8C73",
+      "accent-700": "#70705C",
+      "secondary-400": "#0E0E0C",
+      "secondary-300": "#262626",
+      "secondary-200": "#4D4D4D",
+      "secondary-100": "#666666",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
